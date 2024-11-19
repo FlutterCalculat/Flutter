2469c25790bf0b9b62a35c473f4bfa40d874ef7d
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-11-19 15:25:32 +0900

    change screen name

--- lib/multiplication/screen/multiplication_screen.dart
+++ lib/multiplication/screen/multiplication_screen.dart
@@ -2,7 +2,7 @@
 
 import 'package:adder/view/custom_app_bar.dart';
 import 'package:flutter/material.dart';
-import '../feature/gesture_detector.dart';
+import '../feature/gesture_multiplication.dart';
 
 class MultiplicationScreen extends StatelessWidget {
   @override
@@ -10,7 +10,7 @@ class MultiplicationScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: CustomAppBar(name: "한준혁", type: "곱셈"),
-      body: GestureDetectorExample(),
+      body: GestureMultiplication(),
     );
   }
 }
