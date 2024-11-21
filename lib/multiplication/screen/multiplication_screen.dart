416a285e354915fa6323312364eb6af395e08eaa
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-11-21 14:07:05 +0900

    fix merge comflict

--- lib/multiplication/screen/multiplication_screen.dart
+++ lib/multiplication/screen/multiplication_screen.dart
@@ -1,8 +1,8 @@
 
 
+import 'package:adder/multiplication/feature/multiplication_tab.dart';
 import 'package:adder/view/custom_app_bar.dart';
 import 'package:flutter/material.dart';
-import '../feature/gesture_detector.dart';
 
 class MultiplicationScreen extends StatelessWidget {
   @override
@@ -10,7 +10,7 @@ class MultiplicationScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: CustomAppBar(name: "한준혁", type: "곱셈"),
-      body: GestureDetectorExample(),
+      body: MultiplicationTab(),
     );
   }
 }
